ccfa7337c630aa3ab11c22832290f32c6a86a478
BeatrisFD <48329533+BeatrisFD@users.noreply.github.com>
2021-03-27 13:15:48 -0300

    Update SQLQuery3_Prog_Estruturada.sql

--- SQLQuery3_Prog_Estruturada.sql
+++ SQLQuery3_Prog_Estruturada.sql
@@ -1,8 +1,8 @@
-/*Fazer um algoritmo que leia 3 valores e retorne se os valores formam um triângulo e se ele é
-isóceles, escaleno ou equilátero.
-Condições para formar um triângulo
+/*Fazer um algoritmo que leia 3 valores e retorne se os valores formam um triÃ¢ngulo e se ele Ã©
+isÃ³celes, escaleno ou equilÃ¡tero.
+CondiÃ§Ãµes para formar um triÃ¢ngulo
 	Nenhum valor pode ser = 0
-	Um lado não pode ser maior que a soma dos outros 2.
+	Um lado nÃ£o pode ser maior que a soma dos outros 2.
 */
 declare @A as int
 declare @B as int
@@ -25,19 +25,19 @@ else if @A = @B OR @A = @C OR @B = @C
 	PRINT 'TRIANGULO ISOSCELES'
 
 
- -- Fazer um algoritmo que leia 1 número e mostre se são múltiplos de 2,3,5 ou nenhum deles
+ -- Fazer um algoritmo que leia 1 nÃºmero e mostre se sÃ£o mÃºltiplos de 2,3,5 ou nenhum deles
 declare @Z as int
-set @Z = 53
+set @Z = 55
 if @Z % 2 = 0
-	PRINT 'múltiplo de 2'
+	PRINT 'mÃºltiplo de 2'
 else if @Z % 3 = 0
-	PRINT 'múltiplos de 3'
+	PRINT 'mÃºltiplos de 3'
 else if @Z % 5 = 0
-	PRINT 'múltiplos de 5'
+	PRINT 'mÃºltiplos de 5'
 else 
 	PRINT 'nenhum deles'
 
- -- Fazer um algoritmo que leia 3 número e mostre o maior e o menor
+ -- Fazer um algoritmo que leia 3 nÃºmero e mostre o maior e o menor
 declare @F as int
 declare @G as int
 declare @H as int
@@ -52,27 +52,44 @@ if @G > @maior
 	SELECT @maior = @G
 if @H > @maior
 	SELECT @maior = @H
+	PRINT @maior
 
 set @menor = @F
-if @G < @maior
+if @G < @menor
 	SELECT @menor = @G
-if @H < @maior
+if @H < @menor
 	SELECT @menor = @H
-	PRINT @maior
 	PRINT @menor
 
 /*
-Fazer um algoritmo que calcule os 15 primeiros termos da série
+Fazer um algoritmo que calcule os 15 primeiros termos da sÃ©rie
 1,1,2,3,5,8,13,21,...
 E calcule a soma dos 15 termos
 */
+WITH sequencia_fibonacci(fibonacci_nro, proximo_nro) AS (
+	-- consulta Ã¢ncora
+	SELECT 0, 1
+	UNION ALL
+	-- consulta recursiva
+	SELECT proximo_nro, fibonacci_nro = fibonacci_nro + proximo_nro
+	FROM sequencia_fibonacci
+	WHERE fibonacci_nro < 300
+
+)SELECT SUM(fibonacci_nro) as Soma FROM sequencia_fibonacci;
 
 
--- Fazer um algorimto que separa uma frase, colocando todas as letras em maiúsculo e em minúsculo
+-- Fazer um algorimto que separa uma frase, colocando todas as letras em maiÃºsculo e em minÃºsculo
+SELECT Upper('frase de exemplo') AS maiusculo
 
+SELECT lower('FRASE DE EXEMPLO') as minusculo
 
 -- Fazer um algoritmo que inverta uma palavra
-
+SELECT Reverse('exemplo') AS inverso
 
 -- Verificar palindromo
+DECLARE @string NVARCHAR(255);
+SET @string = 'aaa';
 
+SELECT CASE WHEN @string=REVERSE(@string)THEN 'eh palindromo'
+            ELSE 'nao eh palindromo' 
+            END  
