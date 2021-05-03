7636ece3df07419ea6a9b50f0eb56b4096396b5b
BeatrisFD <48329533+BeatrisFD@users.noreply.github.com>
2021-05-03 13:06:23 -0300

    Add files via upload

---
+++ AvaliaçãoParte2/src/model/JogoQuartas.java
@@ -0,0 +1,29 @@
+package model;
+
+public class JogoQuartas {
+	private String timeA;
+	private String timeB;
+	
+	
+	public JogoQuartas(String timeA, String timeB) {
+		super();
+		this.timeA = timeA;
+		this.timeB = timeB;
+	}
+	
+	
+	public String getTimeA() {
+		return timeA;
+	}
+	public void setTimeA(String timeA) {
+		this.timeA = timeA;
+	}
+	public String getTimeB() {
+		return timeB;
+	}
+	public void setTimeB(String timeB) {
+		this.timeB = timeB;
+	}
+	
+	
+}
